c52d205ff9d722a81b34bdb5316da36ede6e0977
vadim <Lvads@mail.ru>
2024-12-05 16:08:09 +0300

    12_2 done

---
+++ 12/module_12_1.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        run1 = Runner('Test1')
+        for i in range(10):
+            run1.walk()
+        self.assertEqual(run1.distance, 50)
+
+    def test_run(self):
+        run2 = Runner('Test2')
+        for i in range(10):
+            run2.run()
+        self.assertEqual(run2.distance, 100)
+
+    def test_challenge(self):
+        run1 = Runner('Test1')
+        run2 = Runner('Test2')
+        for i in range(10):
+            run1.walk()
+            run2.run()
+        self.assertNotEqual(run1.distance, run2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
