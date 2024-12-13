f9eb57a84ece68781d7c4ecb14478d6232c11d93
bodobondit <bodo93@gmail.com>
2024-12-14 02:40:12 +0300

    Update module_13_3.py

--- 13/module_13_3.py
+++ 13/module_13_3.py
@@ -6,7 +6,7 @@ from aiogram.types import Message
 import asyncio
 from aiogram.fsm.storage.memory import MemoryStorage
 
-api = '7650439552:AAEKdNLhGvfZZoNY3ho_G4Nj3sp4XkLapEI'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(storage=MemoryStorage())
 start_router = Router()
